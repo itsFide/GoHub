730766f760ef283d65d5a34f31d6fc516fd247e5
Sergey Murzak <murzak301@gmail.com>
2020-11-04 21:49:35 +0600

    Адаптив закончен

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@ if(animItems.length > 0){
             const animItem = animItems[i];
             let animItemHeight = animItem.offsetHeight;
             let animItemOffset = offset(animItem).top;
-            let animStart = 4;
+            let animStart = 2;
 
             let animItemPoint = window.innerHeight - animItemHeight / animStart;
             if(animItemHeight > window.innerHeight){
@@ -31,3 +31,19 @@ if(animItems.length > 0){
     },200)
 
 }
+// Burger
+let burgerBtn = document.querySelector('.burger');
+let burgerItem = document.querySelectorAll('.burger__item');
+let introWrapper = document.querySelector('.intro__wrapper');
+let menu = document.querySelector('.mobile__menu');
+burgerBtn.addEventListener('click',function(){
+    burgerItem.forEach(function(event){
+        event.classList.toggle('active');
+        if(event.classList.contains('active')){
+            introWrapper.style.opacity = '0.5';
+        } else {
+            introWrapper.style.opacity = '1';
+        }
+    })
+    menu.classList.toggle('active');
+})
